a72d06876382be8da9549132e24c3f58eb861c71
Samuel-C <samuelcamilo8@gmail.com>
2026-01-25 19:22:44 -0300

    moved stuff to string basic

--- src/string list/string_list.c
+++ src/string list/string_list.c
@@ -1,34 +1,8 @@
-#include <stdlib.h>
 #include "string_list.h"
+
+#include <stdlib.h>
 #include "../debug/debug.h"
-
-int Size(const char* string)
-{
-    int size = 0;
-    while (string[size] != '\0') size++;
-    return size;
-}
-
-char* Copy(const char* string, int size)
-{
-    if (size < 0)
-    {
-        logError("size of string can't be negative");
-        return NULL;
-    }
-
-    char* copy = (char*)malloc((size + 1) * sizeof(char));
-    if (copy == NULL)
-    {
-        logError("error at allocation of new string");
-        return NULL;
-    }
-
-    int index;
-    for(index = 0; index < size; index++) copy[index] = string[index];
-    copy[index] = '\0';
-    return copy;
-}
+#include "../string basic/string_basic.h"
 
 struct string_list_link_struct
 {
--- src/string list/string_list.h
+++ src/string list/string_list.h
@@ -1,18 +1,6 @@
 #ifndef STRING_LIST
 #define STRING_LIST
 
-/**
- * Returns the size of a string.
- */
-int Size(const char* string);
-
-/**
- * Creates a copy of a string.
- * If it fails to allocate memory to the string, it returns NULL.
- * Returns a pointer to the new string.
- */
-char* Copy(const char* string, int size);
-
 typedef struct string_list_link_struct string_list_link;
 
 typedef struct string_list_struct string_list;

3f54d0161c2d54aaf31286e9be39a9e829653f55
Samuel-C <samuelcamilo8@gmail.com>
2026-01-25 21:00:45 -0300

    moved some definitions

--- src/debug/debug.c
+++ src/debug/debug.c
@@ -4,6 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 char* format(const char* fmt, va_list args)
 {
     va_list args_copy;
@@ -26,7 +34,7 @@ void logDebugCaller(const char* file, int line, const char* func, const char* fm
 
     char* msg = format(fmt, args);
 
-    if (DEBUG) printf("%sDEBUG (%s - %s() - line %d): %s%s\n", ANSI_COLOR_BLUE, file, func, line, msg, ANSI_COLOR_RESET);
+    printf("%sDEBUG (%s - %s() - line %d): %s%s\n", ANSI_COLOR_BLUE, file, func, line, msg, ANSI_COLOR_RESET);
 
     free(msg);
 }
--- src/debug/debug.h
+++ src/debug/debug.h
@@ -3,16 +3,12 @@
 
 #define DEBUG 1
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-
-#define logDebug(fmt, ...) \
-    logDebugCaller(__FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+#if DEBUG
+    #define logDebug(fmt, ...) \
+        logDebugCaller(__FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+#else
+    #define logDebug(fmt, ...) ((void)0)
+#endif
 
 #define logWarning(fmt, ...) \
     logWarningCaller(__FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
